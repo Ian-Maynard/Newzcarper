1e7f169fe299810cae64bf869967068680d34f78
Ian-Maynard <maynard.ian@gmail.com>
2019-11-09 22:15:43 -0500

    refactored

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-// var request = require("request");
-// var cheerio = require("cheerio");
+var cheerio = require("cheerio");
 var app = express(); // reference Express 
 const routes = require('./routes/routes');
-
+const scrape = require('./routes/scrape');
 // const router = express.Router(); 
 
 mongoose.Promise = Promise; // configure mongoose for ES6 promises
@@ -19,6 +18,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public")); //Static Directory
 app.use('/', routes);
+app.use('/scrape', scrape);
 
 // Mongoose config and init
 mongoose.connect("mongodb://localhost/scraperdata14", { useNewUrlParser: true, useUnifiedTopology: true } ); // Mongod connection
@@ -34,3 +34,4 @@ db.once("open", function() {
 app.listen(7000, function() {
   console.log("App running on port 7000!");
 });
+
