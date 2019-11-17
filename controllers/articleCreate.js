ad675d8b6cc41abdb775ab24ad8203afc381e77b
Ian-Maynard <maynard.ian@gmail.com>
2019-11-17 18:09:44 -0500

    Modularity 4

--- controllers/articleCreate.js
+++ controllers/articleCreate.js
@@ -1,6 +1,16 @@
 /* jshint esversion: 6 */ 
 /* jshint esversion: 8 */ 
 
+const express = require("express");
+var cheerio = require("cheerio");
+var request = require("request");
+var Bitly = require("bitlyapi");
+var Note = require("../models/note.js");
+var Article = require("../models/Article.js");
+var bitly = new Bitly('fd0a57a9269bf1d523ec4bd38c18f0812c444f04'); // Shorten URL
+const router= express.Router(); 
+
+
 function articleCreat(srce, url, title, link, articles) {
 
       title = title.replace(/\t|\n/g, ""); // strip out certain characters
@@ -10,6 +20,7 @@ function articleCreat(srce, url, title, link, articles) {
             title = title.substring(0, 64); // constrain length of title
           }
         // To create shareable URL - Implement bitly here. 
+
                   var article = new Article();
                       article.source = srce;
                       article.title = title;
