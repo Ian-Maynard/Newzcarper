cea8f3b51087bc8a54dfe177edd5cba195a3046f
Ian-Maynard <maynard.ian@gmail.com>
2019-11-17 19:25:16 -0500

    Modularity 6

--- controllers/articleCreate.js
+++ controllers/articleCreate.js
@@ -6,20 +6,26 @@ var Article = require("../models/Article.js");
 var bitly = new Bitly('fd0a57a9269bf1d523ec4bd38c18f0812c444f04'); // Shorten URL
 const articleCreate = module.exports();
 
-function articleCreat(title, link) {
-// Input: Current title and link for article
+function articleCreat(title, link, urlSwitch, article) {
+// Input: Current title and link for article, and null article
 // Output: a formatted article 
 
       title = title.replace(/\t|\n/g, ""); // strip out certain characters
       title = title.trim(); // Trim Title
 
-          if (title.length > 65) {
+      if (title.length > 65) {
             title = title.substring(0, 64); // constrain length of title
           }
-        // To create shareable URL - Implement bitly here. 
-                  var article = new Article();
-                      article.source = srce;
-                      article.title = title;
-                      article.link = link;
-        return(article);
+                      
+     if (urlSwitch) {
+          link = sURL + link; 
+         }// If URL root is required.
+            // To create shareable URL - Implement bitly here. 
+    // To create shareable URL - Implement bitly here. 
+
+    article.source = srce;
+    article.title = title;
+    article.link = link;
+    console.log("Article Created as   "+link+" *** "+title);
+    return(article);
 }
--- routes/scrape.js
+++ routes/scrape.js
@@ -7,16 +7,16 @@ var request = require("request");
 var Bitly = require("bitlyapi");
 var Note = require("../models/note.js");
 var Article = require("../models/Article.js");
+var urlScraper = require("../controllers/urlScraper.js");
 var bitly = new Bitly('fd0a57a9269bf1d523ec4bd38c18f0812c444f04'); // Shorten URL
 const router= express.Router(); 
 
 router.get("/scrape",  (req, res) =>  {
-  
-                const reuters =  skraper("Reuters", "http://www.reuters.com/", true, ".article-heading");
-                const upi = skraper("UPI","http://www.upi.com/",false,".story");
-                // const deutschWelle = skraper("Deutsche Welle","http://www.dw.com/",true,".news");
-                // const bloomberg = skraper("Bloomberg","https://www.bloomberg.com/",true,".top-news-v3-story-headline");
-                // const time =  skraper("Time","http://www.time.com/",true,".rail-article-title");
+    const reuters =  urlScraper("Reuters", "http://www.reuters.com/", true, ".article-heading");
+    const upi = urlScraper("UPI","http://www.upi.com/",false,".story");
+                // const deutschWelle = urlScraper("Deutsche Welle","http://www.dw.com/",true,".news");
+                // const bloomberg = urlScraper("Bloomberg","https://www.bloomberg.com/",true,".top-news-v3-story-headline");
+                // const time =  urlScraper("Time","http://www.time.com/",true,".rail-article-title");
                 // res.render("../public/index.html",articles);
                       
   });
